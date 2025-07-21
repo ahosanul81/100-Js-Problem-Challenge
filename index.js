d894a5eb1b8503c21f16975417e49dffc6681145
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-07-22 02:48:26 +0600

    solved problem 15-16 = 2  problem

--- index.js
+++ index.js
@@ -325,4 +325,75 @@ const runningSum = (nums) => {
 };
 
 const result14 = runningSum([1, 2, 3, 4]);
-console.log(result14);
+// console.log(result14);
+
+/************Problem- 15**********
+ * 1365. How Many Numbers Are Smaller Than the Current Number
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+ 
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+ * 
+ * 
+ */
+
+const smallerNumbersThanCurrent = (nums) => {
+  let ansArr = [];
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (j !== i && nums[j] < nums[i]) {
+        count += 1;
+      }
+    }
+    ansArr[i] = count;
+    count = 0;
+  }
+
+  return ansArr;
+};
+const result15 = smallerNumbersThanCurrent([8, 1, 2, 2, 3]);
+// console.log(result15);
+
+/***********problem- 16************
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+ */
+
+const defangIPaddr = (address) => {
+  const splittedAddress = address.split("");
+
+  let defangAddress = "";
+  for (let i = 0; i < splittedAddress.length; i++) {
+    if (splittedAddress[i] === ".") {
+      splittedAddress[i] = "[.]";
+    }
+  }
+
+  return splittedAddress.join("");
+};
+const result16 = defangIPaddr("1.1.1.1");
+console.log(result16);
