1ed40f25c948303a77a04fea2f136564cad71120
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-08-05 02:06:22 +0600

    solved problem 48  = 1  problem

--- index.js
+++ index.js
@@ -1509,3 +1509,49 @@ const result47 = majorityElement([1, 3, 1, 1, 4, 1, 1, 5, 1, 1, 6, 2, 2]);
 // const result47 = majorityElement([1]);
 // const result47 = majorityElement([2, 2, 2, 2]);
 // console.log(result47);
+
+/*************problem - 48 ***************
+ * 728. Self Dividing Numbers
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+A self-dividing number is not allowed to contain the digit zero.
+
+Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
+
+ 
+
+Example 1:
+
+Input: left = 1, right = 22
+Output: [1,2,3,4,5,6,7,8,9,11,12,15,22]
+ */
+const selfDividingNumbers = function (left, right) {
+  if (left <= 0) return;
+  let resArr = [];
+
+  for (let i = left; i <= right; i++) {
+    let selfDivisible = true;
+    let loopingValue = "";
+    loopingValue = i.toString();
+
+    for (let j = 0; j < loopingValue.length; j++) {
+      const digit = Number(loopingValue[j]);
+      if (digit === 0 || i % digit !== 0) {
+        selfDivisible = false;
+      }
+    }
+    if (selfDivisible) {
+      resArr.push(i);
+    }
+  }
+  return resArr;
+};
+
+const result48 = selfDividingNumbers(1, 22);
+console.log(result48);
