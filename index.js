ef63e72d5570694a4ce7e2e1d6b6ea8f99ef3575
Md Ahosanul Islam <ahosanul81@gmail.com>
2025-08-10 01:24:33 +0600

    solved  problem 51  = 1  problem

--- index.js
+++ index.js
@@ -1703,6 +1703,44 @@ Input: nums = [1,3,5,6], target = 5
 Output: 2
  */
 
-const searchInsert = function (nums, target) {};
-const result51 = searchInsert();
-console.log(result51);
+const searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
+var searchInsert2 = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target || nums[i] > target) {
+      return i;
+    }
+  }
+
+  return nums.length;
+};
+// const searchInsert = function (nums, target) {
+//   let arr = [...nums];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === target) {
+//       return i;
+//     } else {
+//       arr.push(target);
+//       break;
+//     }
+//   }
+//   return arr.sort().indexOf(target);
+// };
+// const result51 = searchInsert((nums = [1, 3, 5, 6]), (target = 2));
+//  searchInsert2(([1001], 5));
